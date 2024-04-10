9290f63b7caaf9c71b73ebf29b1d61e5ebdb5263
Jagath Nalaka <jagathbotheju@gmail.com>
2024-04-10 18:56:55 +0300

    my-leave-plan-v1.1.4

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,14 +2,27 @@ import Link from "next/link";
 // import AuthButton from "./AuthButton";
 import { ThemeSwitcher } from "./ThemeSwitcher";
 import AuthButton from "./AuthButton";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
-    <div className="flex w-full shadow-lg h-20 dark:bg-slate-700">
+    <div className="flex w-full shadow-lg shadow-sky-600 h-20 dark:bg-slate-700">
       <div className="flex items-center justify-between container max-w-6xl w-full p-5">
-        <Link href="/" className="text-4xl font-bold">
-          My Leave Plan
-        </Link>
+        <div className="flex items-center">
+          <div className="flex w-[150px] h-[100px] relative p-5">
+            <Image
+              src="/images/summer-holidays.png"
+              className="absolute mt-[10px]"
+              alt="logo"
+              // width={150}
+              // height={50}
+              fill
+            />
+          </div>
+          <Link href="/" className="text-4xl font-bold">
+            My Leave Plan
+          </Link>
+        </div>
         <div className="flex items-center gap-4">
           <AuthButton />
           <ThemeSwitcher />
